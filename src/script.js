612a8461a9acef32193a46d80c0cc46b2a3e35b6
mizlin <mizlin@gmail.com>
2016-03-04 10:46:49 +0100

    header and footer included to all the files from external html files except for tips&råd page

--- src/script.js
+++ src/script.js
@@ -5,71 +5,6 @@ var isRated = false;
 $('#votes').text(getLocalStorage("key2"));
 $('#average').text(getLocalStorage("key3"));
 
-// scones
-function displayPortionScones() {
-	portions = getLocalStorage("key1");
-	displaySlider();
-	var text = "person";
-	if (portions == 1) {
-		text = text;
-	} else {
-		text += "s";
-	}
-	document.getElementById("selectedPortion").innerHTML = "for " + portions + " " + text;
-
-	egg();
-	milkScones();
-	sugarScones();
-	salt();
-	bpScones();
-	butterScones();
-}
-window.onload = displayPortion;
-
-function displaySlider() {
-	$("#quantity").val(portions);
-}
-
-function changePortion() {
-	portions = document.getElementById("quantity").value;
-	setLocalStorage("key1", portions);
-	displayPortion();
-}
-
-function eggYolk() {
-	var text = "egg yolk";
-	if (portions == 1) {
-		text = text;
-	} else {
-		text += "s";
-	}
-	document.getElementById("egg_yolk").innerHTML = portions + " " + text;
-}
-
-function milk() {
-	document.getElementById("milk").innerHTML = 25 * portions + "ml full-fat milk";
-}
-
-
-function sugar() {
-	document.getElementById("sugar").innerHTML = 10 * portions + "g sugar plus extra for the topping";
-}
-
-function cream() {
-	document.getElementById("heavy_cream").innerHTML = 100 * portions + "ml heavy cream";
-}
-
-function vanilla() {
-	var text = "vanilla pod";
-	if (portions == 1) {
-		text = text;
-	} else {
-		text += "s";
-	}
-	document.getElementById("vanilla_pod").innerHTML = portions + " " + text;
-}
-
-// creme brulee
 function displayPortion() {
 	portions = getLocalStorage("key1");
 	displaySlider();
@@ -81,13 +16,61 @@ function displayPortion() {
 	}
 	document.getElementById("selectedPortion").innerHTML = "for " + portions + " " + text;
 
+// if scones:
+	eggScones();
+	flourScones();
+	bpScones();
+	saltScones();
+	butterScones();
+	milkScones();
+
+//	if creme_brulee:
+
 	eggYolk();
 	milk();
 	sugar();
 	cream();
 	vanilla();
+
 }
 window.onload = displayPortion;
+
+function eggScones() {
+	var text = "egg";
+	if (portions == 1) {
+		text = text;
+	} else {
+		text += "s";
+	}
+	document.getElementById("egg_scones").innerHTML = portions + " " + text;
+}
+
+function flourScones() {
+	document.getElementById("flour_scones").innerHTML = 500 * portions + "g flour";
+}
+
+function bpScones() {
+	document.getElementById("bp_scones").innerHTML = 2 * portions + "teaspoons baking powder";
+}
+
+function saltScones() {
+	var text = "pinch";
+	if (portions == 1) {
+		text = text;
+	} else {
+		text += "es";
+	}
+	document.getElementById("salt_scones").innerHTML = portions + " " + text + " of salt";
+}
+
+function butterScones() {
+	document.getElementById("butter_scones").innerHTML = 50 * portions + "g butter";
+}
+
+function milkScones() {
+	document.getElementById("milk_scones").innerHTML = 200 * portions + "ml milk";
+}
+
 
 function displaySlider() {
 	$("#quantity").val(portions);
@@ -209,3 +192,15 @@ function setLocalStorage(key, value) {
 		throw "window.localStorage, not defined";
 	}
 }
+
+$(document).ready(function(){
+  $.get("include/header.html", function(data) {
+    $("#header").html(data);
+  });
+});
+
+$(document).ready(function(){
+  $.get("include/footer.html", function(data) {
+    $("#footer").html(data);
+  });
+});
