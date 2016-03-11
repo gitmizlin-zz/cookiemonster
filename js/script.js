232046df68875144cf865af1a3b614d8229f565b
KateGH <kates.epost@gmail.com>
2016-03-11 11:53:50 +0100

    hrefLink

--- js/script.js
+++ js/script.js
@@ -450,8 +450,8 @@ console.log(dailyImage[dailyNr].href);
 var imgSource = dailyImage[dailyNr].src;
 var hrefLink = dailyImage[dailyNr].href;
 
-$("#recepi-of-the-day-img").attr("src", imgSource);
+$("#recepi-of-the-day-img").attr("src", + imgSource);
 $("#recepi-of-the-day-img").attr("href", hrefLink);
 
-document.getElementbyId("dailyLink").href = hrefLink;
+document.getElementById("dailyLink").href = hrefLink;
 }
