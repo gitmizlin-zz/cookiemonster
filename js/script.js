996f095bb7fba4b6dec8760b1852fc9dadcd2fb3
KateGH <kates.epost@gmail.com>
2016-03-11 18:40:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -450,7 +450,7 @@ console.log(dailyImage[dailyNr].href);
 var imgSource = dailyImage[dailyNr].src;
 var hrefSource = dailyImage[dailyNr].href;
 
-document.getElementById("recepi-of-the-day-img").src = imgSource; 
+$("#recepi-of-the-day-img").attr("src", imgSource);
 document.getElementById("dailyLink").href = hrefSource;
 }
 
