c6e6c81e00d017baa0bd3d28043564e71e62303f
KateGH <kates.epost@gmail.com>
2016-03-11 12:00:50 +0100

    imgSource

--- js/script.js
+++ js/script.js
@@ -448,10 +448,11 @@ console.log(dailyImage[dailyNr].src);
 console.log(dailyImage[dailyNr].href); 
 
 var imgSource = dailyImage[dailyNr].src;
-var hrefLink = dailyImage[dailyNr].href;
+var hrefSource = dailyImage[dailyNr].href;
 
 $("#recepi-of-the-day-img").attr("src", imgSource);
-$("#recepi-of-the-day-img").attr("href", hrefLink);
+$("#recepi-of-the-day-img").attr("href", hrefSource);
 
-document.getElementById("dailyLink").href = hrefLink;
+document.getElementById("recepi-of-the-day-img").src = imgSource; 
+document.getElementById("dailyLink").href = hrefSource;
 }
